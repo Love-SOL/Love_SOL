99bfe538b0f9d2989b691541398d0a4b1a64b670
SSuSG <psg980331@naver.com>
2023-09-01 23:37:43 +0900

    feat: Swagger 설정 추가

---
+++ backend/src/main/java/com/ssafy/lovesol/global/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.ssafy.lovesol.global.config;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+/**
+ *  접속 : http://localhost:8080/swagger-ui/index.html
+ */
+
+@Configuration
+
+public class OpenApiConfig {
+	@Bean
+	public OpenAPI openApi() {
+		final String securitySchemeName = "bearerAuth";
+
+		return new OpenAPI()
+				.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+				.components(
+						new Components()
+								.addSecuritySchemes(securitySchemeName,
+										new SecurityScheme()
+												.name(securitySchemeName)
+												.type(SecurityScheme.Type.HTTP)
+												.scheme("bearer")
+												.bearerFormat("JWT")
+								)
+				)
+				.info(
+						new Info()
+								.title("BenePick API Document")
+								.version("v0.0.1")
+								.description("BenePick API 명세서입니다.")
+				);
+	}
+}
