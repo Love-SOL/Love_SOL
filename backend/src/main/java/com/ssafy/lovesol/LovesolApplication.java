b4845d3f8eec922379e69aa7199ffa6fe20cd441
SSuSG <psg980331@naver.com>
2023-09-01 22:14:07 +0900

    init: backend 프로젝트 생성

---
+++ backend/src/main/java/com/ssafy/lovesol/LovesolApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.lovesol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovesolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovesolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/lovesol/LovesolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.lovesol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovesolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
