65b4c99dac6c587dca2da384aa2bef2dee067efb
SSuSG <psg980331@naver.com>
2023-09-01 23:33:42 +0900

    feat: 샘플 요청,반환 DTO 정의

---
+++ backend/src/main/java/com/ssafy/lovesol/domain/schedule/dto/response/SampleResponseDto.java
@@ -0,0 +1,21 @@
+package com.ssafy.lovesol.domain.schedule.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "샘플 반환 DTO")
+public class SampleResponseDto {
+
+    @NotNull
+    @Schema(description = "설명", example = "예시")
+    private String sample;
+
+}
