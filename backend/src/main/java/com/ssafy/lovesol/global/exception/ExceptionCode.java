e825c281bdb09238cb83f55aa947ed41f4cd1e50
SSuSG <psg980331@naver.com>
2023-09-01 23:12:36 +0900

    feat: 공통 예외처리 코드 정의

---
+++ backend/src/main/java/com/ssafy/lovesol/global/exception/ExceptionCode.java
@@ -0,0 +1,20 @@
+package com.ssafy.lovesol.global.exception;
+
+import lombok.Getter;
+
+/**
+ *  ErrorCode는 임의로 지정하였음
+ */
+@Getter
+public enum ExceptionCode {
+
+	SERVER_EXCEPTION(500, "서버에서 예측하지 못한 에러가 발생했습니다.");
+
+	private final int errorCode;
+	private final String errorMessage;
+
+	ExceptionCode(int errorCode, String errorMessage) {
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+}
---
+++ backend/src/main/java/com/ssafy/lovesol/global/exception/ExceptionController.java
@@ -0,0 +1,20 @@
+package com.ssafy.lovesol.global.exception;
+
+import com.ssafy.lovesol.global.response.ResponseResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice(basePackages = "com.ssafy.lovesol")
+public class ExceptionController {
+
+	@ExceptionHandler(Exception.class)
+	public ResponseResult incorrectLanguageCodeException(Exception err) {
+		log.info("Error : {}", err.getClass());
+		log.info("Error Message : {}", err.getMessage());
+		return ResponseResult.exceptionResponse(ExceptionCode.SERVER_EXCEPTION);
+	}
+}
