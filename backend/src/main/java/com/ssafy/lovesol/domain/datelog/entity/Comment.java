0a36c6b5d235cddfa719a6ffad1abad315be81e1
SSuSG <psg980331@naver.com>
2023-09-02 00:59:13 +0900

    feat: Comment Entity 생성

---
+++ backend/src/main/java/com/ssafy/lovesol/domain/datelog/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.ssafy.lovesol.domain.datelog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "image_id", nullable = false)
+    private Image image;
+}
