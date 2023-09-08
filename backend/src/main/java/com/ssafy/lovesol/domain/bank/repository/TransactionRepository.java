b4708ce5234debf8725cd4a4dd5b6392e4bd0aa3
gwonjihun <aspire51@naver.com>
2023-09-08 21:36:45 +0900

    Feat: 결재 내역 조회 TransactionRepository 쿼리 추가(#100)

--- backend/src/main/java/com/ssafy/lovesol/domain/bank/repository/TransactionRepository.java
+++ backend/src/main/java/com/ssafy/lovesol/domain/bank/repository/TransactionRepository.java
@@ -3,9 +3,16 @@ package com.ssafy.lovesol.domain.bank.repository;
 import com.ssafy.lovesol.domain.bank.entity.Account;
 import com.ssafy.lovesol.domain.bank.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
     Optional<Transaction> findByAccountAndDepositAmount(Account account,int depositAmount);
+
+//    @Query(value = "select a from ")
+//    List<Transaction> findAccountLocalDate(Long AccountId, LocalDateTime date);
+    List<Transaction> findTransactionsByTransactionAtGreaterThanEqualAAndAccountEquals(LocalDateTime transactionAt,Account account);
 }
