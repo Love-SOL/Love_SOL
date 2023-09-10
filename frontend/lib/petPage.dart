6c69a7632a12f67123540a519b45009660a04ac7
hyuntall <hyunchul1998@naver.com>
2023-09-11 00:42:19 +0900

    feat: image 경로 assets 추가

--- frontend/lib/petPage.dart
+++ frontend/lib/petPage.dart
@@ -13,13 +13,13 @@ class PetPage extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            icon: Image.asset('personicon.png'), // 사람 모양 아이콘
+            icon: Image.asset('assets/personicon.png'), // 사람 모양 아이콘
             onPressed: () {
               // 아이콘을 눌렀을 때 수행할 작업 추가
             },
           ),
           IconButton(
-            icon: Image.asset('bellicon.png'), // 알림(종 모양) 아이콘
+            icon: Image.asset('assets/bellicon.png'), // 알림(종 모양) 아이콘
             onPressed: () {
               // 아이콘을 눌렀을 때 수행할 작업 추가
             },
