269b463e966e4e3b5bde84113f94c2fb457ea43b
psg980331@naver.com <psg980331@naver.com>
2023-09-16 16:39:47 +0900

    Feat: 알림 API 연결 및 화면 구성

--- frontend/lib/homePage.dart
+++ frontend/lib/homePage.dart
@@ -35,7 +35,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Map<String, dynamic> accountData = {};
   List<PieChartSectionData> sectionList = [];
-
+  List<NoticeResDto> noticeList = [];
 
 
   String userId = "";
@@ -90,6 +90,7 @@ class _HomePageState extends State<HomePage> {
     await _loadUserData(); // 사용자 데이터 로드를 기다립니다.
     await fetchAccountData(); // 초기 데이터 로드를 기다립니다.
     await fetchTransactionCategoryData(accountData["accountNumber"]);
+    await fetchNoticeData();
   }
 
   Future<void> _loadUserData() async {
@@ -151,6 +152,26 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<void> fetchNoticeData() async {
+    final response = await http.get(Uri.parse("http://10.0.2.2:8080/api/notice/$userId"));
+
+    var decode = utf8.decode(response.bodyBytes);
+    Map<String, dynamic> responseBody = json.decode(decode);
+    int statusCode = responseBody['statusCode'];
+
+    if (statusCode == 200) {
+      List<dynamic> data = responseBody['data'];
+      List<NoticeResDto> dataList = data.map((data) => NoticeResDto.fromJson(data as Map<String, dynamic>)).toList();
+
+      setState(() {
+        noticeList = dataList;
+      });
+
+    } else {
+      throw Exception('API 요청 실패');
+    }
+  }
+
   List<PieChartSectionData> createPieChartSections(List<GetTransactionByCategoryResponseDto> data) {
     return data.map((item) {
       print(item);
@@ -199,24 +220,19 @@ class _HomePageState extends State<HomePage> {
                 context: context,
                 builder: (BuildContext context) {
                   return AlertDialog(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
                     title: Text('알림'),
                     content: Container(
                       width: double.maxFinite,
                       height: 300,
-                      child: ListView(
-                        children: [
-                          ListTile(
-                            title: Text('알림 1'),
-                            subtitle: Text('알림 내용 1'),
-                          ),
-                          ListTile(
-                            title: Text('알림 2'),
-                            subtitle: Text('알림 내용 2'),
-                          ),
-                        ],
+                      child: ListView.builder(
+                        itemCount: noticeList.length,
+                        itemBuilder: (context, index) {
+                          final notice = noticeList[index];
+                          return ListTile(
+                            title: Text(notice.title),
+                            subtitle: Text(notice.body),
+                          );
+                        },
                       ),
                     ),
                     actions: [
@@ -832,6 +848,8 @@ class _CouplePageState extends State<CouplePage> {
   String petName = '';
   int petType = 0;
   String dday = '0';
+  String userId = '';
+  String coupleId = '';
   String schedule = '일정이 없어요';
   String scheduleDDay = '';
   Map<String, dynamic> petData = {};
@@ -843,27 +861,32 @@ class _CouplePageState extends State<CouplePage> {
   bool isPaid = false;
   void initState(){
     super.initState();
+    _loadUserData();
     _loadAnniversaryData();
     _loadScheduleData();
     _loadPetData();
   }
 
+  Future<void> _loadUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    userId = (prefs.getInt('userId') ?? '').toString();
+    coupleId = (prefs.getInt('coupleId') ?? '').toString();
+  }
+
   Future<void> _loadAnniversaryData() async {
     final prefs = await SharedPreferences.getInstance();
     final coupleId = prefs.getInt("coupleId");
-    print('커플 페이지');
-    print(coupleId);
     final response = await http.get(Uri.parse("http://10.0.2.2:8080/api/couple/anniversary/$coupleId"));
 
     var decode = utf8.decode(response.bodyBytes);
     Map<String, dynamic> responseBody = json.decode(decode);
     int statusCode = responseBody['statusCode'];
-
     if (statusCode == 200) {
       // 성공
       setState(() {
         dday = responseBody['data'].toString();
       });
+
     } else {
       print(statusCode);
       // 실패
@@ -878,11 +901,15 @@ class _CouplePageState extends State<CouplePage> {
     var decode = utf8.decode(response.bodyBytes);
     Map<String, dynamic> responseBody = json.decode(decode);
     int statusCode = responseBody['statusCode'];
+    print('스테이터스 코드');
+    print(statusCode);
 
     if (statusCode == 200) {
       setState(() {
         scheduleData = Map<String, dynamic>.from(responseBody['data']);
       });
+      print('몇일?');
+      print(scheduleData);
     } else {
       print(statusCode);
       // 실패
@@ -1270,7 +1297,7 @@ class NoticeResDto {
       title: json['title'],
       body: json['body'],
       createAt: DateTime.parse(json['createAt']),
-      senderName: json['SenderName'],
+      senderName: json['senderName'],
     );
   }
 }
--- frontend/lib/widget/Appbar.dart
+++ frontend/lib/widget/Appbar.dart
@@ -1,77 +1,158 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget{
   final String title;
 
   CustomAppBar({required this.title});
 
   @override
+  _CustomAppBarState createState() => _CustomAppBarState();
+
+  @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  List<NoticeResDto> noticeList = [];
+  String userId = '';
+  String coupleId = '';
+
+  @override
+  void initState() {
+    super.initState();
+    print('알림함!!');
+    _loadUserDataAndFetchData();
+  }
+
+  Future<void> _loadUserDataAndFetchData() async {
+    await _loadUserData(); // 사용자 데이터 로드를 기다립니다.
+    await fetchNoticeData();
+  }
+
+  Future<void> _loadUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    userId = (prefs.getInt('userId') ?? '').toString();
+    coupleId = (prefs.getInt('coupleId') ?? '').toString();
+  }
+
+  Future<void> fetchNoticeData() async {
+    final response = await http.get(Uri.parse("http://10.0.2.2:8080/api/notice/$userId"));
+
+    var decode = utf8.decode(response.bodyBytes);
+    Map<String, dynamic> responseBody = json.decode(decode);
+    int statusCode = responseBody['statusCode'];
+
+    print(statusCode);
+    if (statusCode == 200) {
+      List<dynamic> data = responseBody['data'];
+      List<NoticeResDto> dataList = data.map((data) => NoticeResDto.fromJson(data as Map<String, dynamic>)).toList();
+
+      setState(() {
+        noticeList = dataList;
+      });
+      print(noticeList[0].title);
+
+    } else {
+      throw Exception('API 요청 실패');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      automaticallyImplyLeading : false,
-      backgroundColor: Color(0xFFF7F7F7),
-      elevation: 0,
-      iconTheme: IconThemeData(
-        color: Color(0XFF0046FF),
-      ),
-      actions: [
-        IconButton(
-          icon: Image.asset('assets/personicon.png'),
-          onPressed: () {},
+    return PreferredSize(
+      preferredSize: Size.fromHeight(kToolbarHeight),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xFFF7F7F7),
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Color(0XFF0046FF),
         ),
-        IconButton(
-          icon: Image.asset('assets/bellicon.png'),
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('알림'),
-                  content: Container(
-                    width: double.maxFinite,
-                    height: 300,
-                    child: ListView(
-                      children: [
-                        ListTile(
-                          title: Text('알림 1'),
-                          subtitle: Text('알림 내용 1'),
-                        ),
-                        ListTile(
-                          title: Text('알림 2'),
-                          subtitle: Text('알림 내용 2'),
-                        ),
-                      ],
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/personicon.png'),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Image.asset('assets/bellicon.png'),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('알림'),
+                    content: Container(
+                      width: double.maxFinite,
+                      height: 300,
+                      child: ListView.builder(
+                        itemCount: noticeList.length,
+                        itemBuilder: (context, index) {
+                          final notice = noticeList[index];
+                          return ListTile(
+                            title: Text(notice.title),
+                            subtitle: Text(notice.body),
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('닫기'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
-      ],
-      title: Padding(
-        padding: const EdgeInsets.only(top: 0),
-        child: Text(
-          title,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('닫기'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+        title: Padding(
+          padding: const EdgeInsets.only(top: 0),
+          child: Text(
+            widget.title,  // widget을 사용하여 title에 접근
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
+        centerTitle: true,
       ),
-      centerTitle: true,
+    );
+  }
+}
+
+class NoticeResDto {
+  final String kind;
+  final String title;
+  final String body;
+  final DateTime createAt;
+  final String senderName;
+
+  NoticeResDto({
+    required this.kind,
+    required this.title,
+    required this.body,
+    required this.createAt,
+    required this.senderName,
+  });
+
+  // JSON에서 NoticeResDto로 변환하는 팩토리 생성자
+  factory NoticeResDto.fromJson(Map<String, dynamic> json) {
+    return NoticeResDto(
+      kind: json['kind'],
+      title: json['title'],
+      body: json['body'],
+      createAt: DateTime.parse(json['createAt']),
+      senderName: json['senderName'],
     );
   }
 }
