fa6f99abb94073494327ebec8e1ee9b97b629aa2
hyuntall <hyunchul1998@naver.com>
2023-09-16 14:46:13 +0900

    feat: 커플 통장 상대방 정보 등록 API 연동

--- frontend/lib/coupleSettingPage.dart
+++ frontend/lib/coupleSettingPage.dart
@@ -14,7 +14,10 @@ class Couplesettingpage extends StatefulWidget {
 class _CouplesettingpageState extends State<Couplesettingpage> {
   bool isAutoTransferEnabled = false;
   String userId = "";
-
+  String receiverId = "";
+  String aniversary = "";
+  String day = "";
+  String amount = "";
   @override
   void initState() {
     super.initState();
@@ -49,6 +52,32 @@ class _CouplesettingpageState extends State<Couplesettingpage> {
     userId = (prefs.getInt('userId') ?? '').toString();
   }
 
+  Future<void> sendCoupleNotice() async {
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8080/api/couple/connect'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(<String, dynamic>{
+          'id': userId,
+          "receiverId": receiverId,
+          "aniversary": aniversary,
+          "day": day,
+          "amount": amount
+        }),
+      );
+      // 응답 데이터(JSON 문자열)를 Dart 맵으로 파싱
+      Map<String, dynamic> responseData = json.decode(response.body);
+      // 파싱한 데이터에서 필드에 접근
+      int statusCode = responseData['statusCode'];
+      print(responseData);
+      print("안녕하세요우");
+    } catch (e) {
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,21 +95,35 @@ class _CouplesettingpageState extends State<Couplesettingpage> {
             SizedBox(height: 20),
             Column(
               children: [
-                buildInputBox('상대방 ID', '아이디를 입력해주세요'),
+                buildInputBox('상대방 ID', '아이디를 입력해주세요', onChanged: (value) {
+                  receiverId = value;
+                }),
                 SizedBox(height: 10),
-                buildInputBox('기념일', '기념일을 입력해주세요'),
+                buildInputBox('기념일', '기념일을 입력해주세요', onChanged: (value) {
+                  aniversary = value;
+                }),
                 SizedBox(height: 10),
-                buildInputBox('자동이체 날짜', '날짜를 선택해주세요'),
+                buildInputBox('자동이체 날짜', '날짜를 선택해주세요', onChanged: (value) {
+                  day = value;
+                }),
                 SizedBox(height: 20),
-                buildInputBox('자동이체 금액', '금액을 선택해주세요'),
+                buildInputBox('자동이체 금액', '금액을 선택해주세요', onChanged: (value) {
+                  amount = value;
+                }),
                 SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     ElevatedButton(
-                      onPressed: () {
+                      onPressed: () async {
+                        print(receiverId);
+                        print(aniversary);
+                        print(day);
+                        print(amount);
+                        if (receiverId == "" || aniversary == "" || day == "" || amount == "") return;
+                        await sendCoupleNotice();
                         Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => HomePage2(),
+                          builder: (context) => CouplePage(),
                         ));
                       },
                       child: Text(
@@ -105,7 +148,7 @@ class _CouplesettingpageState extends State<Couplesettingpage> {
     );
   }
 
-  Widget buildInputBox(String label, String hintText, {TextEditingController? controller}) {
+  Widget buildInputBox(String label, String hintText, {TextEditingController? controller, required Null Function(dynamic value) onChanged}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -131,156 +174,7 @@ class _CouplesettingpageState extends State<Couplesettingpage> {
           ),
           child: TextFormField(
             controller: controller,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.all(16.0),
-              hintText: hintText,
-              hintStyle: TextStyle(color: Colors.grey),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-
-class Couplesettingpage2 extends StatelessWidget {
-  final TextEditingController birthdateController = TextEditingController();
-  final TextEditingController verificationCodeController = TextEditingController();
-  final TextEditingController accountNumberController = TextEditingController(); // 계좌번호 컨트롤러 추가
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0XFF0046FF),
-        elevation: 0,
-      ),
-      body: Container(
-        color: Color(0xFFF7F7F7),
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            buildInputBox('이름', '이름을 입력하세요'),
-            SizedBox(height: 20),
-            buildInputBox('생년월일', '숫자 6자리 입력', controller: birthdateController),
-            SizedBox(height: 20),
-            buildInputBox('계좌번호', '12자리 입력', controller: accountNumberController),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('인증번호 보내기'),
-                      content: Text('여기에 인증번호 보내기 내용을 입력하세요.'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(); // 대화 상자를 닫음
-                          },
-                          child: Text('닫기'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Align(
-                alignment: Alignment.centerRight, // 텍스트를 오른쪽으로 정렬
-                child: Text(
-                  '인증번호 보내기 >',
-                  style: TextStyle(
-                    color: Color(0xFF777777),
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-            ),
-            buildInputBox('인증번호', '숫자 6자리 입력', controller: verificationCodeController),
-            SizedBox(height: 20),
-            SizedBox(height: 80),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigate back to the previous page (MainPage)
-                    Navigator.of(context).pop();
-                  },
-                  child: Text(
-                    '이전',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFFDADADA), // 버튼 배경색
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    minimumSize: Size(120, 48), // 버튼 크기 설정
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigate to SignUpPage2
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => HomePage2(),
-                    ));
-                  },
-                  child: Text(
-                    '확인',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0XFF0046FF), // 버튼 배경색
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    minimumSize: Size(120, 48), // 버튼 크기 설정
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildInputBox(String label, String hintText, {TextEditingController? controller}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        SizedBox(height: 8),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                offset: Offset(0, 2),
-                blurRadius: 4.0,
-              ),
-            ],
-          ),
-          child: TextFormField(
-            controller: controller,
-            inputFormatters: [
-              LengthLimitingTextInputFormatter(label == '계좌번호' ? 12 : 6), // 최대 길이 설정
-              FilteringTextInputFormatter.digitsOnly, // 숫자만 입력 허용
-            ],
-            keyboardType: TextInputType.number, // 숫자 키보드 설정
+            onChanged: onChanged,
             decoration: InputDecoration(
               border: InputBorder.none,
               contentPadding: EdgeInsets.all(16.0),
